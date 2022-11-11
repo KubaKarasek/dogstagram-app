be1e3eb042b85ea315ca6a5817fea77825a64e06
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2022-11-12 00:51:27 +0100

    fix: app header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     
-  title: string = 'Psintagram';
+  title: string = 'Dogstagram';
   
   constructor() { }
 
